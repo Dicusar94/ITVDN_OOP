912cfc195daacdb214820665b9e85bf136a7bdec
Dicusar Denis <dicusar.dv@gmail.com>
2022-11-03 12:16:08 +0200

    PartialClasses

---
+++ 2.ClassesAndOjects/001_PartialClasses/PartialClasses/PartialClass_1.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _001_PartialClasses.PartialClasses;
+
+public partial class PartialClass_1
+{
+    public void MethodFromPart1()
+    {
+        Console.WriteLine("I Part");
+    }
+}
---
+++ 2.ClassesAndOjects/001_PartialClasses/Program.cs
@@ -0,0 +1,13 @@
+﻿using _001_PartialClasses.PartialClasses;
+
+namespace _001_PartialClasses;
+
+public static class Program
+{
+    private static void Main()
+    {
+        var partialClass = new PartialClass_1();
+        partialClass.MethodFromPart1();
+        partialClass.MethodFromPart2();
+    }
+}
