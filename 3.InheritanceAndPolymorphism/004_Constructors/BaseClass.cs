5b806d36111ddcf1531fbb2530d81f9769d9d5c6
Dicusar Denis <dicusar.dv@gmail.com>
2022-11-08 20:33:28 +0200

    constructors

---
+++ 3.InheritanceAndPolymorphism/004_Constructors/BaseClass.cs
@@ -0,0 +1,15 @@
+namespace _004_Constructors;
+
+class BaseClass
+{
+    public readonly int baseNumber;
+
+    public BaseClass()
+    {
+    }
+
+    public BaseClass(int baseNumber)
+    {
+        this.baseNumber = baseNumber;
+    }
+}
